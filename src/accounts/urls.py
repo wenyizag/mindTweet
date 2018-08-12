d005635eb4042ae018188c3558e62832815203b3
Wenyi Zhang <wenyizag@outlook.com>
2018-08-12 17:05:35 -0400

    toggle follow

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from django.views.generic.base import RedirectView
 
 from .views import (
-    UserDetailView
+    UserDetailView, UserFollowView,
     )
 
 urlpatterns = [
@@ -11,6 +11,7 @@ urlpatterns = [
     # url(r'^search/$', TweetListView.as_view(), name='list'), # /tweet/
     # url(r'^create/$', TweetCreateView.as_view(), name='create'), # /tweet/create/
     url(r'^(?P<username>[\w.@+-]+)/$', UserDetailView.as_view(), name='detail'), # /tweet/1/
+    url(r'^(?P<username>[\w.@+-]+)/follow/$', UserFollowView.as_view(), name='follow'), # /tweet/1/
     # url(r'^(?P<pk>\d+)/update/$', TweetUpdateView.as_view(), name='update'), # /tweet/1/update/
     # url(r'^(?P<pk>\d+)/delete/$', TweetDeleteView.as_view(), name='delete'), # /tweet/1/delete/
 ]

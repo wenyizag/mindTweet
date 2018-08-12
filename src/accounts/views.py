dfd27bc2fbdc7975d108b9919ce11226b6bb7dc1
Wenyi Zhang <wenyizag@outlook.com>
2018-08-12 17:15:05 -0400

    post_save signal

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.views.generic import DetailView
 from django.views import View
+from django.db.models.signals import post_save
 # Create your views here.
 
 from .models import UserProfile
@@ -29,3 +31,8 @@ class UserFollowView(View):
 			is_following = UserProfile.objects.toggle_follow(request.user, toggle_user)
 		return redirect("profile:detail", username=username)
 
+def post_save_user_receiver(sender, instance, created, *args, **kwargs):
+	if created:
+		new_profile = UserProfile.objects.get_or_create(user=instance)
+
+post_save.connect(post_save_user_receiver, sender=settings.AUTH_USER_MODEL)
